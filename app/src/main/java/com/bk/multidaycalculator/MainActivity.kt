6c5fa98b0a6066aeebe35527f373c84014a4bc95
mejdi14 <mejdiclubiste@gmail.com>
2022-08-16 11:59:21 +0100

    first part is ready now let us go into the real shit

--- app/src/main/java/com/bk/multidaycalculator/MainActivity.kt
+++ app/src/main/java/com/bk/multidaycalculator/MainActivity.kt
@@ -20,38 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize().background(Color(0xFFf3e8e6))) {
-
-                Column(modifier = Modifier.fillMaxSize()) {
-                Box(modifier = Modifier.height(80.dp))
-                    Text(
-                        text = "20 Days",
-                        modifier = Modifier
-                            .padding(start = 20.dp),
-                        color = Color(0xFF59CE8F),
-                        fontSize = 40.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Box(modifier = Modifier.height(20.dp))
-                    Text(
-                        text = "On avarage you need to ride",
-                        modifier = Modifier
-                            .padding(start = 20.dp),
-                        color = Color.Black,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 14.sp
-                    )
-                    Text(
-                        text = "4 hours and 33 minutes",
-                        modifier = Modifier
-                            .padding(start = 20.dp),
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp
-                    )
-                    Box(modifier = Modifier.height(20.dp))
-                }
-            }
+            MainScreen()
         }
     }
 }
--- app/src/main/java/com/bk/multidaycalculator/MainScreen.kt
+++ app/src/main/java/com/bk/multidaycalculator/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.bk.multidaycalculator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,26 +8,73 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen() {
-    Column (modifier = Modifier.fillMaxSize()){
-        Text(text = "20 Days", modifier = Modifier
-            .size(30.dp)
-            .padding(start = 20.dp)
-            , color = Color(0xFF59CE8F)
-            , fontWeight = FontWeight.Bold)
-        Box(modifier = Modifier.height(20.dp))
-        Text(text = "On avarage you need to ride", modifier = Modifier
-            .size(14.dp)
-            .padding(start = 20.dp)
-            , color = Color(0xFF000000)
-            , fontWeight = FontWeight.Medium)
-        Text(text = "4 hours and 33 minutes", modifier = Modifier
-            .size(20.dp)
-            .padding(start = 20.dp)
-            , color = Color(0xFF000000)
-            , fontWeight = FontWeight.Bold)
-        Box(modifier = Modifier.height(20.dp))
+    Box(modifier = Modifier.fillMaxSize().background(Color(0xFFf3e8e6))) {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.height(60.dp))
+            Text(
+                text = "20 Days",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color(0xFF7DCE13),
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Box(modifier = Modifier.height(20.dp))
+            Text(
+                text = "On avarage you need to ride",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "4 hours and 33 minutes",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Box(modifier = Modifier.height(20.dp))
+            Text(
+                text = "On some days, you'll have to climb over",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "466 meters",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Box(modifier = Modifier.height(20.dp))
+            Text(
+                text = "The overall difficulty is",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "Intermediate",
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
     }
 }
