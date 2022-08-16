e8e977d17d47a34ed768c2b50102144aeaa32245
mejdi14 <mejdiclubiste@gmail.com>
2022-08-16 11:41:56 +0100

    the first ui attempt

--- app/src/main/java/com/bk/multidaycalculator/MainScreen.kt
+++ app/src/main/java/com/bk/multidaycalculator/MainScreen.kt
@@ -1,14 +1,10 @@
 package com.bk.multidaycalculator
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -19,6 +15,17 @@ fun MainScreen() {
             .size(30.dp)
             .padding(start = 20.dp)
             , color = Color(0xFF59CE8F)
+            , fontWeight = FontWeight.Bold)
+        Box(modifier = Modifier.height(20.dp))
+        Text(text = "On avarage you need to ride", modifier = Modifier
+            .size(14.dp)
+            .padding(start = 20.dp)
+            , color = Color(0xFF000000)
+            , fontWeight = FontWeight.Medium)
+        Text(text = "4 hours and 33 minutes", modifier = Modifier
+            .size(20.dp)
+            .padding(start = 20.dp)
+            , color = Color(0xFF000000)
             , fontWeight = FontWeight.Bold)
         Box(modifier = Modifier.height(20.dp))
     }
